121b8cbc97c5950f5ede5544f65a3801722e2b27
Malibin <nightmare73@naver.com>
2020-05-14 17:55:40 +0900

    [working] 카탈로그 한글 이름 바인딩 완료

---
+++ Crawling/src/main/kotlin/basedb/Catalog.kt
@@ -0,0 +1,56 @@
+package basedb
+
+import java.io.File
+import java.lang.StringBuilder
+
+val catalogTranslateList = listOf(
+    "가방", "도구", "러그", "모자", "바닥", "벽지", "사진", "상의", "신발", "액세서리"
+    , "양말", "우산", "울타리", "음악", "포스터", "하의", "한벌옷"
+)
+
+//가구 : 벽걸이/잡화
+fun main() {
+
+//    bindNameKor("음악","음악")
+    for (kind in catalogTranslateList) {
+        bindNameKor(kind, kind)
+    }
+    bindFurnitureNameKor()
+}
+
+fun bindFurnitureNameKor() {
+    bindNameKor("가구", "가구")
+    bindNameKor("벽걸이", "가구")
+    bindNameKor("잡화", "가구")
+}
+
+fun bindNameKor(kind: String, translateKind: String) {
+    val stringBuilder = StringBuilder()
+    val translateMap = getTranslateMap(translateKind)
+    File("raw\\catalog", "$kind.csv").forEachLine {
+        val nameEng = it.split(",")[0]
+        val nameKor = translateMap[nameEng]
+        stringBuilder
+            .append(nameKor).append(",")
+            .append(it).append("\n")
+
+        if (nameKor == null) {
+            println("kind = $kind : $nameKor,$it\n")
+        }
+    }
+
+    val outputFile = File("fixed_data", "catalog_$kind.txt")
+    outputFile.writeText(stringBuilder.toString())
+    println("Write Success")
+}
+
+
+fun getTranslateMap(kind: String): Map<String, String> {
+    val map = HashMap<String, String>()
+    map["Name"] = "nameKor"
+    File("raw\\catalog\\번역", "${kind}번역.csv").forEachLine {
+        val cursor = it.split(",")
+        map[cursor[2]] = cursor[14]
+    }
+    return map
+}
